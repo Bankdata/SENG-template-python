db8c91fefae58764209c1dcb844b577a402802b9
Casper Thule <c.thulehansen@live.com>
2022-11-09 14:38:18 +0100

    Updated test

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -5,6 +5,9 @@ class TestRectangle(unittest.TestCase):
     def test_area(self):
         self.assertEqual(Rectangle(4.0, 3.0).get_area(), 12.0, "Should be 12.0")
 
+    def test_area2(self):
+        self.assertEqual(Rectangle(43.0, 3.0).get_area(), 12.0, "Should be 12.0")    
+
 
 if __name__ == '__main__':
     unittest.main()
