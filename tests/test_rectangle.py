81f95ca9e0eaa133185da0fa9dac28baa90ccc1c
Casper Thule <c.thulehansen@live.com>
2022-11-09 14:52:02 +0100

    Replaed nose with pytest

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -1,13 +1,8 @@
-import unittest
 from rectangle import Rectangle
-class TestRectangle(unittest.TestCase):
+class TestRectangle():
 
     def test_area(self):
-        self.assertEqual(Rectangle(4.0, 3.0).get_area(), 12.0, "Should be 12.0")
+        assert Rectangle(4.0, 3.0).get_area() == 12.0
 
     # def test_area2(self):
     #     self.assertEqual(Rectangle(43.0, 3.0).get_area(), 12.0, "Should be 12.0")    
-
-
-if __name__ == '__main__':
-    unittest.main()
